da7d2f4ddb17e109c47ec6ecc1133d812a125a60
Gareth Rushgrove <gareth@morethanseven.net>
2017-09-16 18:28:40 +0100

    Update homebrew package to 0.7.0

--- Formula/kubeval.rb
+++ Formula/kubeval.rb
@@ -1,9 +1,9 @@
 class Kubeval < Formula
   desc "Validate your Kubernetes configurations"
   homepage "https://github.com/garethr/"
-  url "https://github.com/garethr/kubeval/releases/download/0.6.0/kubeval-darwin-amd64.tar.gz"
-  sha256 "40122CCE2ABE1B51EA9752FF00FEE61AE3107623D66D7FFC776C8EDAE8FA9B91".downcase
-  version "0.6.0"
+  url "https://github.com/garethr/kubeval/releases/download/0.7.0/kubeval-darwin-amd64.tar.gz"
+  sha256 "A12D2644E73CDBBE36FE4BAB46E4CED0F8B6D1F1CF6BAFB635BACD6C2CD63F10".downcase
+  version "0.7.0"
 
   bottle :unneeded
 
