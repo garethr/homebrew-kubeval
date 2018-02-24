087245f74c9d90fce466331c4645456f905fda32
Gareth Rushgrove <gareth@morethanseven.net>
2018-02-24 19:07:52 +0000

    updated for 0.7.1

--- Formula/kubeval.rb
+++ Formula/kubeval.rb
@@ -3,7 +3,7 @@ class Kubeval < Formula
   homepage "https://github.com/garethr/"
   url "https://github.com/garethr/kubeval/releases/download/0.7.0/kubeval-darwin-amd64.tar.gz"
   sha256 "A12D2644E73CDBBE36FE4BAB46E4CED0F8B6D1F1CF6BAFB635BACD6C2CD63F10".downcase
-  version "0.7.0"
+  version "0.7.1"
 
   bottle :unneeded
 
