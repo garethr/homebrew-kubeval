eb9f57b1ef6d08fe2b9366c7b67fb6ba1aeca98a
Gareth Rushgrove <gareth@morethanseven.net>
2018-02-24 19:37:36 +0000

    correct SHA for release

--- Formula/kubeval.rb
+++ Formula/kubeval.rb
@@ -2,7 +2,7 @@ class Kubeval < Formula
   desc "Validate your Kubernetes configurations"
   homepage "https://github.com/garethr/"
   url "https://github.com/garethr/kubeval/releases/download/0.7.0/kubeval-darwin-amd64.tar.gz"
-  sha256 "A12D2644E73CDBBE36FE4BAB46E4CED0F8B6D1F1CF6BAFB635BACD6C2CD63F10".downcase
+  sha256 "0E9FBC4495CE048936F6A535A48DB9F1164404DE68B7A04EB806A0FCA7212485".downcase
   version "0.7.1"
 
   bottle :unneeded
