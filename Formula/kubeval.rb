fd91729c0331cffb15095ecbb454329c58c24855
Gareth Rushgrove <gareth@morethanseven.net>
2018-02-24 19:52:45 +0000

    correct download URL for version of kubeval

--- Formula/kubeval.rb
+++ Formula/kubeval.rb
@@ -1,7 +1,7 @@
 class Kubeval < Formula
   desc "Validate your Kubernetes configurations"
   homepage "https://github.com/garethr/"
-  url "https://github.com/garethr/kubeval/releases/download/0.7.0/kubeval-darwin-amd64.tar.gz"
+  url "https://github.com/garethr/kubeval/releases/download/0.7.1/kubeval-darwin-amd64.tar.gz"
   sha256 "0E9FBC4495CE048936F6A535A48DB9F1164404DE68B7A04EB806A0FCA7212485".downcase
   version "0.7.1"
 
