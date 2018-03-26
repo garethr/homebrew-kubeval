b9ecd4f2e59de420289f25f0b4473473e1f2a663
Omer Katz <omer.drow@gmail.com>
2018-03-26 11:40:01 +0300

    Support Linuxbrew

--- Formula/kubeval.rb
+++ Formula/kubeval.rb
@@ -1,8 +1,13 @@
 class Kubeval < Formula
   desc "Validate your Kubernetes configurations"
   homepage "https://github.com/garethr/"
-  url "https://github.com/garethr/kubeval/releases/download/0.7.1/kubeval-darwin-amd64.tar.gz"
-  sha256 "0E9FBC4495CE048936F6A535A48DB9F1164404DE68B7A04EB806A0FCA7212485".downcase
+  if OS.mac?
+    url "https://github.com/garethr/kubeval/releases/download/0.7.1/kubeval-darwin-amd64.tar.gz"
+    sha256 "0E9FBC4495CE048936F6A535A48DB9F1164404DE68B7A04EB806A0FCA7212485".downcase
+  elsif OS.linux?
+    url "https://github.com/garethr/kubeval/releases/download/0.7.1/kubeval-linux-amd64.tar.gz"
+    sha256 "8259D462BD19E5FC2DB2EA304E51ED4DB928BE4343F6C9530F909DBA66E15713".downcase
+  end
   version "0.7.1"
 
   bottle :unneeded
