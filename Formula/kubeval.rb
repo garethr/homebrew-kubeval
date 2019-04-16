5ed216b76ad443aba8d25b779b9ae1e156f03078
Gareth Rushgrove <gareth@morethanseven.net>
2019-04-16 20:03:04 +0100

    bump to 0.8.0 release

--- Formula/kubeval.rb
+++ Formula/kubeval.rb
@@ -1,9 +1,9 @@
 class Kubeval < Formula
   desc "Validate your Kubernetes configurations"
   homepage "https://github.com/garethr/"
-  url "https://github.com/garethr/kubeval/releases/download/0.7.3/kubeval-darwin-amd64.tar.gz"
-  sha256 "efb298bfea2b78a19db559eab620e0318c6a22a9b975d2756221b9fed16f709c".downcase
-  version "0.7.3"
+  url "https://github.com/garethr/kubeval/releases/download/0.8.0/kubeval-darwin-amd64.tar.gz"
+  sha256 "22763d893ea9e9524d98165865dc4697818597a0979f40ddeb1b98dd1ad37094".downcase
+  version "0.8.0"
 
   bottle :unneeded
 
